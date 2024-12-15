c3e27a3031e616f8f1a74575a505d794853a8fce
Elton Rexha <elton.rexha123@outlook.com>
2024-12-15 17:16:21 +0100

    display workspaces on sidebar

--- src/routes/dashboardRouter.js
+++ src/routes/dashboardRouter.js
@@ -1,8 +1,9 @@
 const { Router } = require('express');
-const {isAuth} = require('../middlewares/authMiddleware');
+const { isAuth } = require('../middlewares/authMiddleware');
+const { getWorkspaces } = require('../middlewares/workspaceMiddleware.js');
 const { getAllFilesPage } = require('../controllers/dashboardController');
 const router = Router();
 
-router.get('/home/allFiles', isAuth, getAllFilesPage);
+router.get('/home/allFiles', isAuth, getWorkspaces, getAllFilesPage);
 
 module.exports = router;
--- src/routes/workspaceRouter.js
+++ src/routes/workspaceRouter.js
@@ -3,6 +3,6 @@ const { isAuth } = require('../middlewares/authMiddleware');
 const { createWorkspace } = require('../controllers/workspaceController');
 const router = Router();
 
-router.post('/create', isAuth, createWorkspace)
+router.post('/create', isAuth, createWorkspace);
 
 module.exports = router;
